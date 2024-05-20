648d9eb94e63a8f5e266933e2b210748bcac48cf
Mariia Borisova <mashatenkova61@gmail.com>
2024-05-20 18:48:33 +0500

    Написаны два класса представления (ListView, DetailView) обрабатывающие страницу, где все цветы и страницу с одним выбранным растением.

--- ShopFlowers/flowers/urls.py
+++ ShopFlowers/flowers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 import flowers.views as flowers
 
 urlpatterns = [
-    path('', flowers.MainPage.as_view()),
-    path('flowers/', flowers.FlowersView.as_view()),
+    path('', flowers.MainPage.as_view(), name='main'),
+    path('flowers/', flowers.FlowersView.as_view(), name='flowers'),
+    path('flowers/<slug:slug>/', flowers.FlowerDetailView.as_view(), name='flower'),
 ]
--- ShopFlowers/flowers/views.py
+++ ShopFlowers/flowers/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from .models import Flowers, Category
 from datetime import datetime
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 
 class MainPage(View):
@@ -18,6 +18,16 @@ class FlowersView(ListView):
     model = Flowers
     template_name = 'flowers.html'
     context_object_name = 'flowers'
+
+    def get_queryset(self):
+        return Flowers.objects.all()
+
+
+class FlowerDetailView(DetailView):
+    model = Flowers
+    template_name = 'flower.html'
+    context_object_name = 'flower'
+    slug_url_kwarg = 'slug'
 
     def get_queryset(self):
         return Flowers.objects.all()
